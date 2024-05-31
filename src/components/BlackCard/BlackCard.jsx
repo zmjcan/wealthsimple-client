855410034f66f47884449fdc65b205bfa5aaaefa
“Maggie <mzhang.ark@gmail.com>
2024-05-31 12:29:13 -0400

    added flipcard styles

--- src/components/BlackCard/BlackCard.jsx
+++ src/components/BlackCard/BlackCard.jsx
@@ -1,11 +1,31 @@
 import "./BlackCard.scss";
+import ReactFlipCard from "reactjs-flip-card";
 
-export default function BlackCard({ Title }) {
+export default function BlackCard({ Title, Description }) {
+  const styles = {
+    cardF: {
+      background: "#32302F",
+      color: "#FFFFFF",
+      borderRadius: 90,
+      width: 180,
+      height: 90,
+    },
+    cardB: {
+      background: "#E1DCD0",
+      color: "#32302F",
+      borderRadius: 90,
+      width: 180,
+      height: 90,
+    },
+  };
+
   return (
-    <>
-      <article className="blackcard">
-        <h3>{Title}</h3>
-      </article>
-    </>
+    <ReactFlipCard
+      className="flipcard"
+      frontStyle={styles.cardF}
+      backStyle={styles.cardB}
+      frontComponent={<div className="flipcard__title">{Title}</div>}
+      backComponent={<div className="flipcard__description">{Description}</div>}
+    />
   );
 }
--- src/pages/SeventhPage/SeventhPage.jsx
+++ src/pages/SeventhPage/SeventhPage.jsx
@@ -6,22 +6,31 @@ export default function SeventhPage() {
   return (
     <div>
       <div className="sev-title">
-        <h2>select your hero: (current age)</h2>
-        <h2>(current age)</h2>
+        <h2>Check out your next mission:</h2>
+        <h2>(flip cards to find out more)</h2>
       </div>
       <section className="sev-container">
         {/* need to map here, Link below should be Link to "/8/:id" once data is connected */}
         <Link to="/8">
-          <BlackCard Title="Bat Mobile: TFSA" />
+          <BlackCard Title="Bat Mobile: TFSA" Description="TFSA is a Canadian investment account that allows individuals to earn tax-free income on contributions." />
         </Link>
         <Link to="/8">
-          <BlackCard Title="Side Kick: Cash Account" />
+          <BlackCard
+            Title="Side Kick: Cash Account"
+            Description="Cash account is a basic investment account that allows you to buy/sell securities using  the available cash balance."
+          />
         </Link>
         <Link to="/8">
-          <BlackCard Title="Create My Own Squad (Browse All Options)" />
+          <BlackCard
+            Title="Create My Own Squad (Browse All Options)"
+            Description="Create Own Squad - WealthSimple provides all varieties of options that you could browse from"
+          />
         </Link>
         <Link to="/3">
-          <BlackCard Title="New Origin Story?" />
+          <BlackCard
+            Title="New Origin Story?"
+            Description="This will lead you back to select a new origin story, ready for your next adventure?"
+          />
         </Link>
       </section>
     </div>
